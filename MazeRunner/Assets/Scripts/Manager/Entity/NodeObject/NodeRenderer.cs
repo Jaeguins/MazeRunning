843f32d25e6b1599db642ba7068839d0972b51ca
Jaeguins <jsu4838@naver.com>
2020-05-01 19:11:10 +0900

    벽/바닥 텍스쳐 일치화, 밝게 변경

--- MazeRunner/Assets/Scripts/Manager/Entity/NodeObject/NodeRenderer.cs
+++ MazeRunner/Assets/Scripts/Manager/Entity/NodeObject/NodeRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Manager.Entity.NodeObject
@@ -15,8 +16,15 @@ namespace Assets.Scripts.Manager.Entity.NodeObject
             Manager = parent;
             transform.position = parent.NodePos;
             Walls=new WallRenderer[4];
-            for (int i = 0; i < 4; i++)
+            List<int>walls=new List<int>(4);
+            if(Manager.X==0)walls.Add(3);
+            if(Manager.Y==0)walls.Add(0);
+            walls.Add(1);
+            walls.Add(2);
+            
+            foreach (int i in walls)
             {
+                
                 if (Manager.Wall[i])
                 {
                     Walls[i] = Instantiate(scene.GetWallPrefab(), transform).Initialize(this,i);
